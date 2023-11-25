8076c39a2d8f4f1b348f443f9daa9d8af2171431
PacifistRam <141394720+PacifistRam@users.noreply.github.com>
2023-11-25 12:58:06 +0530

    completed all calculator functions and added styles

--- script/script.js
+++ script/script.js
@@ -5,8 +5,10 @@ const displaySecondary = document.querySelector('#second-ds');
 
 const numberButtons = document.querySelectorAll('.no-btn')
 const operatorButtons = document.querySelectorAll('.operator-btn')
+const dotButton = document.querySelector('.dot-btn')
 const equalToButton = document.querySelector('.equal-btn')
 const clearButton = document.querySelector('.clear-btn')
+const signButton = document.querySelector('.sign-btn')
 
 
 let numberOne = 0;
@@ -14,18 +16,24 @@ let numberTwo = 0;
 let result = 0;
 let operator;
 let operatorClicked = false;
+let error = false;
 
 displayPrimary.value = "";
 
 numberButtons.forEach(button => {
     
-    button.addEventListener('click',(e) => {
+   if(!error)
+    { button.addEventListener('click',(e) => {
         if (operatorClicked) {
             clearText();
             operatorClicked = false;
         }
         displayPrimary.value += button.textContent
-    })
+    })}
+    else {
+        clearText ()
+        clearField()
+    }
 })
 
 operatorButtons.forEach(button => {
@@ -36,12 +44,12 @@ operatorButtons.forEach(button => {
             if (displayPrimary.value != "") {
 
                 if(numberOne === 0) {
-                    numberOne = parseInt(displayPrimary.value)
+                    numberOne = parseFloat(displayPrimary.value)
                     operatorClicked = true;
                 
                 }
                 else if (numberOne != 0 && !operatorClicked) {
-                    numberTwo = parseInt(displayPrimary.value)
+                    numberTwo = parseFloat(displayPrimary.value)
                     operatorClicked = true;
                     calculate(operator);
                     numberOne = result;
@@ -56,50 +64,94 @@ operatorButtons.forEach(button => {
 
 })
             
-       
+ dotButton.addEventListener('click',() => {
+    if (!error) {
+        if (!displayPrimary.value.includes('.')) {
+            displayPrimary.value += '.';
+        }
+    }
+
+ })      
 
 equalToButton.addEventListener('click', () => {
-    calculate(operator);
+    if(displayPrimary.value != "")
+    {
+        calculate(operator);
+    }
+
 })
 clearButton.addEventListener('click' , () => {
     clearText();
     clearField();
 })
 
+signButton.addEventListener('click', () => {
+    if (!error) {
+        if (operatorClicked) {
+            clearText();
+            operatorClicked = false;
+        }
+
+        // Toggle the sign of the displayed number
+        displayPrimary.value = displayPrimary.value.startsWith('-')
+            ? displayPrimary.value.slice(1)
+            : '-' + displayPrimary.value;
+    }
+});
+
+function formatNumber(num) {
+    const formattedNum = parseFloat(num).toString();
+    const [integerPart, decimalPart] = formattedNum.split('.');
+
+    if (decimalPart && parseInt(decimalPart) !== 0) {
+        return formattedNum;
+    } else {
+        return integerPart;
+    }
+}
+
+
 function calculate(operator)
 {   
-   
-    numberTwo = parseInt(displayPrimary.value);
-    switch(operator) {
-        case '+':
-            result = numberOne + numberTwo;
-            displayPrimary.value = result;
-            break;
-        
-        case '-':
-            result = numberOne - numberTwo;
-            displayPrimary.value = result;
-            break;
-        case '*':
-            result = numberOne * numberTwo;
-            displayPrimary.value = result;
-            break;
-        case '/':
-            if (numberTwo === 0)
-            {
-                displayPrimary.value = "ERROR";
-                clearField();
+    if (!error)
+    {
+
+        numberTwo = parseFloat(displayPrimary.value);
+        switch(operator) {
+            case '+':
+                result = numberOne + numberTwo;
+               
+                break;
+            
+            case '-':
+                result = numberOne - numberTwo;
+               
+                break;
+            case '*':
+                result = numberOne * numberTwo;
+              
+                break;
+            case '/':
+                if (numberTwo === 0)
+                {
+                    displayPrimary.value = "ERROR";
+                    clearField();
+                    error = true;
+                    break;
+                }
+                else
+                {
+                    result = numberOne / numberTwo;
+                   
+                }
+                break;
             }
-            else
-            {
-                result = numberOne / numberTwo;
-                displayPrimary.value = result;
-            }
-            break;
-        }
-        numberTwo = 0;
-        numberOne = 0;
-        operatorClicked = true;
+            displayPrimary.value = formatNumber(result.toFixed)(10);
+            numberTwo = 0;
+            numberOne = 0;
+            operatorClicked = true;
+    }
+
 }
                 
 function clearText (){
@@ -112,4 +164,6 @@ function clearField () {
     numberTwo = 0;
     result = 0;
     operatorClicked = false;
+    error = false;
 }
+
